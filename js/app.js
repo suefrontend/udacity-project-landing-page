207acddafbc742a9404c229df7df29409aea20f4
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-03-04 14:04:45 -0800

    refactor: Add getBoundingClientRect() method to check if target is in viewport

--- js/app.js
+++ js/app.js
@@ -13,164 +13,202 @@
  *
  */
 
-window.addEventListener('DOMContentLoaded', () => {
-	/**
-	 * Comments should be present at the beginning of each procedure and class.
-	 * Great to have comments before crucial code sections within the procedure.
-	 */
-	/**
-	 * Define Global Variables
-	 *
-	 */
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+/**
+ * Define Global Variables
+ *
+ */
 
-	const header = document.querySelector('.page__header');
-	const ul = document.getElementById('navbar__list');
-	const sections = Array.from(document.querySelectorAll('[data-nav]'));
-	const containers = document.querySelectorAll("[id*='section'");
-	const footer = document.querySelector('.page__footer');
-	const scrollToTopBtn = document.querySelector('.back-to-top-btn');
-	const collapsibleBtn = document.querySelectorAll('.collapsible-btn');
-	const collapsibleText = Array.from(
-		document.querySelectorAll('.collapsible__content')
-	);
-	const chevron = Array.from(document.querySelectorAll('.fa'));
+const header = document.querySelector('.page__header');
+const ul = document.getElementById('navbar__list');
+// const sections = Array.from(document.querySelectorAll('[data-nav]'));
+const sections = document.querySelectorAll('section');
+const footer = document.querySelector('.page__footer');
+const scrollToTopBtn = document.querySelector('.back-to-top-btn');
+const collapsibleBtn = document.querySelectorAll('.collapsible-btn');
+const collapsibleText = Array.from(
+	document.querySelectorAll('.collapsible__content')
+);
+const chevron = Array.from(document.querySelectorAll('.fa'));
 
-	/**
-	 * End Global Variables
-	 * Start Helper Functions
-	 *
-	 */
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
 
-	const viewportTop = sections.map((section) => {
-		const viewportOffset = section.getBoundingClientRect();
-		return viewportOffset.top + window.scrollY;
-	});
-	const getNavHeight = () =>
-		document.querySelector('.navbar__menu').clientHeight;
-	/**
-	 * End Helper Functions
-	 * Begin Main Functions
-	 *
-	 */
+// const viewportTop = sections.map((section) => {
+// 	const viewportOffset = section.getBoundingClientRect();
+// 	return viewportOffset.top + window.scrollY;
+// });
+// const getNavHeight = () => document.querySelector('.navbar__menu').clientHeight;
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
 
-	// Build menu
-	const createList = (elem) => {
-		const li = document.createElement('li');
+// Build menu
+const createList = (elem) => {
+	const li = document.createElement('li');
 
-		const link = document.createElement('a');
-		link.href = elem.id;
-		link.classList.add('menu__link');
-		link.setAttribute('href', `#${elem.id}`);
-		link.innerHTML = elem.attributes[1].textContent;
+	const link = document.createElement('a');
+	link.href = elem.id;
+	link.classList.add('menu__link');
+	link.setAttribute('href', `#${elem.id}`);
+	link.innerHTML = elem.attributes[1].textContent;
 
-		li.appendChild(link);
+	li.appendChild(link);
 
-		ul.appendChild(li);
-	};
+	ul.appendChild(li);
+};
 
-	sections.forEach((section) => {
-		createList(section);
-	});
+sections.forEach((section) => {
+	createList(section);
+});
 
-	// Add class 'active' to section when near top of viewport
-	// Set sections as active
-	let options = {
-		threshold: 1,
-	};
+// Add class 'active' to section when near top of viewport
+// Set sections as active
+// let options = {
+// 	threshold: 1,
+// };
 
-	const addActiveClass = (containers) => {
-		containers.forEach((container) => {
-			container.target.style.paddingTop = getNavHeight;
+// const addActiveClass = (containers) => {
+// 	containers.forEach((container, index) => {
+// 		container.target.style.paddingTop = getNavHeight;
 
-			if (container.isIntersecting) {
-				container.target.classList.add('your-active-class');
-			}
-			if (!container.isIntersecting) {
-				container.target.classList.remove('your-active-class');
-			}
-		});
-	};
-	const sectionObserver = new IntersectionObserver(addActiveClass);
-	containers.forEach((container) => {
-		sectionObserver.observe(container);
-	});
-	/**
-	 * End Main Functions
-	 * Begin Events
-	 *
-	 */
+// 		if (container.isIntersecting) {
+// 			//	if (index === 0) return null;
+// 			container.target.classList.add('your-active-class');
+// 		}
+// 		if (!container.isIntersecting) {
+// 			container.target.classList.remove('your-active-class');
+// 		}
+// 	});
+// };
+// const sectionObserver = new IntersectionObserver(addActiveClass);
+// containers.forEach((container) => {
+// 	sectionObserver.observe(container);
+// });
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
 
-	// Scroll to anchor ID using scrollTO event
-	// Scroll to section on link click
-	const menuLink = Array.from(document.querySelectorAll('.menu__link'));
-	menuLink.forEach((el, index) => {
-		el.addEventListener('click', (e) => {
-			e.preventDefault();
-			window.scrollTo({
-				top: viewportTop[index],
-				behavior: 'smooth',
-			});
-		});
-	});
-
-	// Hide fixed navigation bar while NOT scrolling
-	window.addEventListener('scroll', (e) => {
-		let lastScroll = 0;
-		const currentScroll = window.pageYOffset;
-
-		if (currentScroll <= 0) {
-			header.classList.remove('hidden');
-			header.classList.add('show');
-		}
-
-		if (currentScroll > lastScroll) {
-			header.classList.remove('hidden');
-			header.classList.add('show');
-			setTimeout(function () {
-				header.classList.remove('show');
-			}, 3000);
-			header.classList.add('hidden');
-		}
-	});
-
-	// Go to top button
-	const goToTop = (footers) => {
-		footers.forEach((footer) => {
-			if (footer.isIntersecting) {
-				// Show go to top button
-				scrollToTopBtn.classList.remove('hidden');
-				scrollToTopBtn.classList.add('show');
-			} else {
-				// Hide go to top button
-				scrollToTopBtn.classList.add('hidden');
-				scrollToTopBtn.classList.remove('show');
-			}
-		});
-	};
-
-	const scrollToTop = () => {
+// Scroll to anchor ID using scrollTO event
+// Scroll to section on link click
+const menuLink = Array.from(document.querySelectorAll('.menu__link'));
+menuLink.forEach((el, index) => {
+	el.addEventListener('click', (e) => {
+		e.preventDefault();
 		window.scrollTo({
-			top: 0,
+			top: viewportTop[index],
 			behavior: 'smooth',
 		});
-	};
-	scrollToTopBtn.addEventListener('click', scrollToTop);
+	});
+});
 
-	let observerGototop = new IntersectionObserver(goToTop);
-	observerGototop.observe(footer);
+// Hide fixed navigation bar while NOT scrolling
+window.addEventListener('scroll', (e) => {
+	let lastScroll = 0;
+	const currentScroll = window.pageYOffset;
 
-	// Make contents collapsible
-	collapsibleBtn.forEach((el, index) => {
-		el.addEventListener('click', function (e) {
-			collapsibleText[index].classList.toggle('show');
+	if (currentScroll <= 0) {
+		header.classList.remove('hidden');
+		header.classList.add('show');
+	}
 
-			if (e.target.classList.contains('fa-chevron-down')) {
-				e.target.classList.remove('fa-chevron-down');
-				e.target.classList.add('fa-chevron-up');
-			} else {
-				e.target.classList.remove('fa-chevron-up');
-				e.target.classList.add('fa-chevron-down');
-			}
-		});
+	if (currentScroll > lastScroll) {
+		header.classList.remove('hidden');
+		header.classList.add('show');
+		setTimeout(function () {
+			header.classList.remove('show');
+		}, 3000);
+		header.classList.add('hidden');
+	}
+});
+
+// Go to top button
+const goToTop = (footers) => {
+	footers.forEach((footer) => {
+		if (footer.isIntersecting) {
+			// Show go to top button
+			scrollToTopBtn.classList.remove('hidden');
+			scrollToTopBtn.classList.add('show');
+		} else {
+			// Hide go to top button
+			scrollToTopBtn.classList.add('hidden');
+			scrollToTopBtn.classList.remove('show');
+		}
+	});
+};
+
+const scrollToTop = () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
+	});
+};
+scrollToTopBtn.addEventListener('click', scrollToTop);
+
+let observerGototop = new IntersectionObserver(goToTop);
+observerGototop.observe(footer);
+
+// Make contents collapsible
+collapsibleBtn.forEach((el, index) => {
+	el.addEventListener('click', function (e) {
+		collapsibleText[index].classList.toggle('show');
+
+		if (e.target.classList.contains('fa-chevron-down')) {
+			e.target.classList.remove('fa-chevron-down');
+			e.target.classList.add('fa-chevron-up');
+		} else {
+			e.target.classList.remove('fa-chevron-up');
+			e.target.classList.add('fa-chevron-down');
+		}
+	});
+});
+
+// const box = document.getElementById('section1');
+// console.log('box', box);
+
+// Check it element is in viewport
+const isInViewport = (element) => {
+	const rect = element.getBoundingClientRect();
+
+	// console.log('window.innerWidth', window.innerWidth);
+	// console.log('window.innerHeight', window.innerHeight);
+	// console.log('rect', rect);
+
+	return (
+		rect.left >= 0 &&
+		rect.top >= 0 &&
+		rect.right <= window.innerWidth &&
+		rect.bottom <= window.innerHeight
+	);
+};
+
+// const setActiveSection = sections.forEach((section, index) => {
+// 	// if (isInViewport(section)) {
+// 	// console.log(`Section ${index} is in viewport`);
+// 	console.log('isInViewport(section)', isInViewport(section));
+// 	// }
+// });
+
+// isInViewport(box);
+
+// console.log('isInViewport(box)', isInViewport(box));
+
+window.addEventListener('scroll', () => {
+	sections.forEach((section, index) => {
+		if (isInViewport(section)) {
+			section.classList.add('your-active-class');
+		} else {
+			section.classList.remove('your-active-class');
+		}
 	});
 });
